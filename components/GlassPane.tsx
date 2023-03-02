583cfb0119f85757c08a257200278ef86e23d21d
Chris Andrews <andrewsc33@icloud.com>
2023-03-02 15:45:45 -0800

    init

---
+++ components/GlassPane.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import clsx from "clsx";
+
+
+const GlassPane = ({ children, className }: any) => {
+  return (
+    <div
+      className={clsx(
+        "glass rounded-2xl border-solid border-2 border-gray-200",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GlassPane;
