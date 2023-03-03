79e1d3217e8949fdee9f52a47df0fa5eea723a1c
Chris Andrews <andrewsc33@icloud.com>
2023-03-03 11:09:20 -0800

    added header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/global.css";
 import GlassPane from "@/components/GlassPane";
 import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
 
 export default function DashboardRootLayout({
   children,
@@ -11,7 +12,12 @@ export default function DashboardRootLayout({
       <body className="w-screen h-screen candy-mesh">
         <GlassPane className="flex items-center w-full h-full ">
           <Sidebar />
-          {children}
+          <div className=" w-full h-full  items-stretch justify-center min-h-[content]">
+            <div className="flex flex-grow">
+              <Header />
+            </div>
+            {children}
+          </div>
         </GlassPane>
         <div id="modal" />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -26,13 +26,14 @@ export default async function Page() {
   return (
     <div className="w-full h-screen pr-6 overflow-y-auto">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
-        <div className="flex grow">
+        {/*<div className="flex grow">
           <Header />
         </div>
+        */}
         <div className="flex flex-wrap items-center justify-center mt-3 -m-3 flex-3 h-content grow">
           {cards.map((card) => (
             <Link href={card.link} key={card.link}>
-              <Card className="flex flex-col items-center justify-center w-64 h-64 m-3 bg-slate-100 hover:bg-slate-600 hover:text-white transition duration-200">
+              <Card className="flex flex-col items-center justify-center w-64 h-64 m-3 bg-slate-100 hover:scale-105 transition duration-200">
                 <h2 className="text-3xl font-bold">{card.title}</h2>
               </Card>
             </Link>
