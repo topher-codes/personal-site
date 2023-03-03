41f5912dc47f519bdd497e45cfedb1ca874a72db
Chris Andrews <andrewsc33@icloud.com>
2023-03-02 19:46:35 -0800

    search modified

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,20 @@
 import Card from "./Card";
 import Input from "./Input";
+import Button from "./Button";
+import { XSquare } from "react-feather";
 
 const Search = () => {
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <Card className="flex flex-col items-center justify-center w-full h-full bg-slate-100">
-      <h1 className="text-4xl font-bold">Search</h1>
+      <div className="flex flex-row justify-between items-between">
+        <h1 className="text-4xl font-bold">Search</h1>
+        <Button className="" size="small" onClick={handleClick}>
+          <XSquare />
+        </Button>
+      </div>
       <Input className="" />
     </Card>
   );
