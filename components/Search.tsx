f322de5720d09cd70058ebd302f47d1c49873d7f
Chris Andrews <andrewsc33@icloud.com>
2023-03-08 11:56:23 -0800

    changed search name

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,23 @@
+"use client";
 import Card from "./Card";
 import Input from "./Input";
 import Button from "./Button";
 import { XSquare } from "react-feather";
+import { useState, useEffect } from "react";
+import { searchBlogPosts } from "@/lib/Search";
 
 const Search = ({ handleClick }) => {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (input.length > 0) {
+      const results = searchBlogPosts(input);
+      setResults(results);
+    } else {
+      setResults([]);
+    }
+  }, [input]);
   return (
     <div className="flex flex-col items-center justify-center w-full h-full ">
       <div className="flex flex-row justify-between items-between">
@@ -12,7 +26,22 @@ const Search = ({ handleClick }) => {
           <XSquare onClick={handleClick} />
         </div>
       </div>
-      <Input className="" />
+      <Input
+        className=""
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {results.length > 0 && (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+          {results?.map((result) => (
+            <Card
+              key={result.slug}
+            >
+              <h1 className="text-2xl font-bold">{result.title}</h1>
+              <p className="text-lg">{result.description}</p>
+            </Card>
+          ))}
+
     </div>
   );
 };
