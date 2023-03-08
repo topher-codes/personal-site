1b35a702217224c447eb182481fe028163830d14
Chris Andrews <andrewsc33@icloud.com>
2023-03-08 13:49:40 -0800

    undid changed

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,13 +4,13 @@ import Input from "./Input";
 import Button from "./Button";
 import { XSquare } from "react-feather";
 import { useState, useEffect } from "react";
-import { searchBlogPosts } from "@/lib/SearchHelper";
 
 const Search = ({ handleClick }) => {
   const [input, setInput] = useState("");
   const handleChange = (e) => {
     setInput(e.target.value);
   };
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full ">
       <div className="flex flex-row justify-between items-between">
