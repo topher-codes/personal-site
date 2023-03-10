b4733c9fb57aa67ddfe584af299fa0e434378eb9
Chris Andrews <andrewsc33@icloud.com>
2023-03-10 08:10:13 -0800

    sorting posts by date

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -27,21 +27,27 @@ const BlogPage = async () => {
   return (
     <div className="flex flex-col items-center w-full h-full overflow-y-auto">
       <h1 className="py-6 text-4xl font-bold">Blog</h1>
-      {posts.map((post) => (
-        <Card key={post.slug} className="w-1/2 p-6 m-6">
-          <p className="text-3xl font-bold text-slate-600">
-            {post.frontmatter.title}
-          </p>
-          <p className="py-4">{post.frontmatter.description}</p>
-          <p>{post.frontmatter.publishedAt}</p>
-          <Link
-            href={`/blog/${post.slug}`}
-            className="flex flex-col items-center w-full"
-          >
-            <Button size="small">Read More</Button>
-          </Link>
-        </Card>
-      ))}
+      {posts
+        .sort(
+          (a, b) =>
+            new Date(b.frontmatter.publishedAt).getTime() -
+            new Date(a.frontmatter.publishedAt).getTime()
+        )
+        .map((post) => (
+          <Card key={post.slug} className="w-1/2 p-6 m-6">
+            <p className="text-3xl font-bold text-slate-600">
+              {post.frontmatter.title}
+            </p>
+            <p className="py-4">{post.frontmatter.description}</p>
+            <p>{post.frontmatter.publishedAt}</p>
+            <Link
+              href={`/blog/${post.slug}`}
+              className="flex flex-col items-center w-full"
+            >
+              <Button size="small">Read More</Button>
+            </Link>
+          </Card>
+        ))}
       <div id="modal" />
     </div>
   );
